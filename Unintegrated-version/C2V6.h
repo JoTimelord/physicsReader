3c264a2c9dc4df8416a7e3c8b0fe53f0ab4caab4
JoTimelord <70925470+JoTimelord@users.noreply.github.com>
2021-06-01 19:22:23 -0700

    Add files via upload

---
+++ Unintegrated-version/C2V6.h
@@ -0,0 +1,32 @@
+#ifndef C2V6_h
+#define C2V6_h
+
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TFile.h"
+#include "TMath.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TBranch.h"
+#include "Math/LorentzVector.h"
+#include <Math/GenVector/GenVector_exception.h>
+#include <Math/GenVector/LorentzVector.h>
+#include <Math/GenVector/PxPyPzE4D.h>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <math.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Declare helper functions
+ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float>> leading(vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float>>> v1);
+ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float>> subleading(vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float>>> v1);
+
+int main(int argc, char** argv);
+void C2V6();
+
+#endif
